519eda6e70a27a177d99e23b8bd51908a37fc93d
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2011-04-26 22:44:04 +0100

    added more examples

--- examples.py
+++ examples.py
@@ -48,7 +48,7 @@ sys_dict = {
         angle = 120,
         initial_angle = 0,
         dist = 10,
-        age = 5,
+        age = 7,
         turtle = turtle.TurtlePIL,
         lsystem = lsystem.LSystem,
         filetype = 'png'
@@ -60,7 +60,7 @@ sys_dict = {
         angle = 60,
         initial_angle = -60,
         dist = 10,
-        age = 5,
+        age = 7,
         turtle = turtle.TurtlePIL,
         lsystem = lsystem.LSystem,
         filetype = 'png'
@@ -88,7 +88,32 @@ sys_dict = {
         turtle = turtle.TurtlePIL,
         lsystem = lsystem.StochasticLSystem,
         filetype = 'png'
+    ),
+
+    'stoch-symtree': Systems(
+        axiom = 'F',
+        rules = { 'F': [ 'FF-[-F+F+F]+[+F-F-F]', 'FF+[+F-F-F]-[-F+F+F]' ] },
+        angle = 22.5,
+        initial_angle = -90,
+        dist = 10,
+        age = 5,
+        turtle = turtle.TurtlePIL,
+        lsystem = lsystem.StochasticLSystem,
+        filetype = 'png'
+    ),
+
+    'stoch-example': Systems(
+        axiom = 'F',
+        rules = { 'F': [ 'F[+F]F[-F]F', 'F[+F]F', 'F[-F]F' ] },
+        angle = 22.5,
+        initial_angle = -90,
+        dist = 10,
+        age = 5,
+        turtle = turtle.TurtlePIL,
+        lsystem = lsystem.StochasticLSystem,
+        filetype = 'png'
     )
+
 }
 
 def generator ( name, sys_dict ):
